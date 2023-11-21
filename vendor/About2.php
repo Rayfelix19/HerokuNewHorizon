016c9990146c41a75c35559ab82f7c57197ae793
aniq <75451490+aniqabidaa@users.noreply.github.com>
2023-11-20 22:46:05 -0500

    Test

---
+++ vendor/About2.php
@@ -0,0 +1,47 @@
+
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<h1>Welcome, Admin!</h1>
+<p>This is your admin dashboard. You have access to various administrative tasks:</p>
+<ul>
+    <li><a href="ManageUser.html">Manage Users</a></li>
+    <li><a href="ManageContent.html">Manage Content</a></li>
+    <li><a href="ViewReports.html">View Reports</a></li>
+    <li><a href="AccountSetting.html">Account Settings</a></li>
+    <li><a href="Logout.html">Logout</a></li>
+</ul>
+
+<h2>Quick Actions</h2>
+<ul>
+    <li><a href="create_user.html">Create New User</a></li>
+    <li><a href="create_content.html">Create New Content</a></li>
+    <li><a href="generate_report.html">Generate Report</a></li>
+    <li><a href="change_password.html">Change Password</a></li>
+</ul>
+
+<h2>Manage Users</h2>
+<ul>
+    <li><a href="user_list.html">View User List</a></li>
+    <li><a href="edit_user.html">Edit User Profiles</a></li>
+    <li><a href="delete_user.html">Delete User Accounts</a></li>
+</ul>
+
+<h2>Content Management</h2>
+<ul>
+    <li><a href="content_list.html">View Content List</a></li>
+    <li><a href="edit_content.html">Edit Content</a></li>
+    <li><a href="delete_content.html">Delete Content</a></li>
+</ul>
+</body>
+</html>
